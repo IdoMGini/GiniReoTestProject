0893bc91d4524aa0d5a7d53495949f9c628b90c9
Ido Meirov <idom@gini-apps.com>
2020-02-13 10:27:48 +0200

    prepare repo 0.1.0

---
+++ Example/Pods/Target Support Files/GiniReoTestProject/GiniReoTestProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GiniReoTestProject : NSObject
+@end
+@implementation PodsDummy_GiniReoTestProject
+@end
---
+++ GiniReoTestProject/Classes/MyClass.swift
@@ -0,0 +1,20 @@
+//
+//  MyClass.swift
+//  GiniReoTestProject
+//
+//  Created by Ido Meirov on 13/02/2020.
+//
+
+import Foundation
+
+public class MyClass
+{
+    public let name: String
+    public let age: Int
+    
+    public init(name: String, age: Int)
+    {
+        self.name   = name
+        self.age    = age
+    }
+}
